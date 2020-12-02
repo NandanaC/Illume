d3791d486172d94109bce437cfc76765fb6b1a74
Nandana <74108378+NandanaC@users.noreply.github.com>
2020-12-02 21:02:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Body from './Components/Body1';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Styles/Footer.css";
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import InstagramIcon from '@material-ui/icons/Instagram';
+
+function Footer(){
+    return(
+        <div className="footer">
+            <div className="inner_footer">
+                <div className="logo_container_footer">
+                    <img src="./logo_illume1.png" alt="logo"></img>
+                </div>
+                <div className="footer_third">
+                    <h1>Need help?</h1>
+                    <a href="#">Terms and Conditions </a>
+                    <a href="#">Terms and conditions </a>
+                </div>
+                <div className="footer_third">
+                    <h1>More</h1>
+                    <a href="#">Privacy policy </a>
+                    <a href="#">Privacy policy </a>
+                </div>
+                <div className="footer_third">
+                    <h1>Follow Us</h1>
+                    <li><a href="#"><i className="Facebook"><FacebookIcon /></i></a></li>
+                    <li><a href="#"><i className="Twitter"><TwitterIcon /></i></a></li>
+                    <li><a href="#"><i className="Instagram"><InstagramIcon /></i></a></li>
+                
+                <span>
+                    xyz
+                </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Footer;
