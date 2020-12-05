890ee14b430657b6fcfc155b60070ce93e3a0fab
Nandana <74108378+NandanaC@users.noreply.github.com>
2020-12-05 18:37:58 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Body from './Components/Body1';
+import NewBlog from './Components/NewBlog';
+import SignLog from './Components/SignLog';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Body />
+      {/*<SignLog />*/}
       <Footer />
+      {/* <NewBlog />  */}
     </div>
   );
 }
