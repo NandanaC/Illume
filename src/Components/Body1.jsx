5897d94f89af9838bf7d507c8ec82fc7747e58e0
Nandana <74108378+NandanaC@users.noreply.github.com>
2020-12-07 22:08:31 +0530

    Add files via upload
    Body and new blog page updated. Removed dialog

--- src/Components/Body1.jsx
+++ src/Components/Body1.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Styles/Body1.css";
 import Card from "./Cards1";
 import NewBlog from './NewBlog';
-import Dialog from '@material-ui/core/Dialog';
+{/*import Dialog from '@material-ui/core/Dialog';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -13,9 +13,9 @@ import Slide from '@material-ui/core/Slide';
 
 const useStyles = makeStyles(theme => ({
     dialogPaper: {
-        height: '95vh',
+        height : '95vh',
         maxWidth: 'none',
-        width: '80%',
+        width:'80%',
     },
     appbar: {
         position: 'relative',
@@ -25,103 +25,26 @@ const useStyles = makeStyles(theme => ({
         flex: 1,
     },
 }));
-
-const Transition = React.forwardRef(function Transition(props, ref) {
-    return <Slide direction="up" ref={ref} {...props} />;
-});
-
-function FullScreenDialog() {
-    const classes = useStyles();
-    const [open, setOpen] = React.useState(false);
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-
-    return (
-        <div className="body" style={open ? { filter: 'blur(2px)' } : { filter: 'none' }}>
-            <div className="left">
-                <button type='button' onClick={handleClickOpen}><a href="#" className="newPost">New Post</a></button>
-            </div>
-            <div className="middle">
-                <div className="blogCard"> <Card /></div>
-                <div className="blogCard"> <Card /></div>
-                <div className="blogCard"> <Card /></div>
-
-            </div>
-            <div className="right"> </div>
-
-            <Dialog className="dialogClass" fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
-                <AppBar className={classes.appBar} style={{ backgroundColor: 'rgb(62, 182, 182)' }}>
-                    <Toolbar>
-                        <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
-                            <CloseIcon />
-                        </IconButton>
-                        <Typography variant="h6" className={classes.title}>
-                            New Blog
-            </Typography>
-                    </Toolbar>
-                </AppBar>
-                <NewBlog />
-            </Dialog>
-        </div>
-    );
-}
-
-export default FullScreenDialog;
-
-
-/* import React, { useState } from "react";
-import "./Styles/Body1.css";
-import Card from "./Cards1";
-import NewBlog from './NewBlog';
-import Dialog from '@material-ui/core/Dialog';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import CloseIcon from '@material-ui/icons/Close';
-import Slide from '@material-ui/core/Slide';
-
-const useStyles = makeStyles(theme => ({
-    dialogPaper: {
-        height: '95vh',
-        maxWidth: 'none',
-        width: '80%',
-    },
-    appbar: {
-        position: 'relative',
-    },
-    title: {
-        marginLeft: theme.spacing(2),
-        flex: 1,
-    },
-}));
-const Transition = React.forwardRef(function Transition(props, ref) {
+const Transition = React.forwardRef(function Transition(props,ref){
     return <Slide direction="up" ref={ref} {...props} />;
 
 });
 export default function FullScreenDialog() {
     const classes = useStyles();
     const [open, setOpen] = React.useState(false);
-
+  
     const handleClickOpen = () => {
-        setOpen(true);
+      setOpen(true);
     };
-
+  
     const handleClose = () => {
-        setOpen(false);
-    };
-
-    return (
-        <div className="body" style={open ? { filter: 'blur(2px)' } : { filter: 'none' }}>
+      setOpen(false);
+    };*/}
+function Body() {
+    return(
+        <div className="body" /*style={open?{filter:'blur(2px)'}:{filter:'none'}}*/>
             <div className="left">
-                <button type='button' onClick={handleClickOpen}><a href="#" className="newPost">New Post</a></button>
+            <a href="#" className="newPost"><button type='button' onClick={()=><NewBlog />}>New Post</button></a>            
             </div>
             <div className="middle">
                 <div className="blogCard"> <Card /></div>
@@ -135,19 +58,20 @@ export default function FullScreenDialog() {
             <div className="right">
 
             </div>
-            <Dialog className="dialogClass" fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
-                <AppBar className={classes.appBar} style={{ backgroundColor: '#18181a' }}>
-                    <Toolbar>
-                        <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
-                            <CloseIcon />
-                        </IconButton>
-                        <Typography variant="h6" className={classes.title}>
-                            New Blog
+                {/*<Dialog className= "dialogClass" fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
+                    <AppBar className={classes.appBar} style={{ backgroundColor: '#18181a'}}>
+                        <Toolbar>
+                            <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
+                                <CloseIcon />
+                            </IconButton>
+                            <Typography variant="h6" className={classes.title}>
+                                New Blog
                             </Typography>
-                    </Toolbar>
-                </AppBar>
-                <NewBlog />
-            </Dialog>
-        </div>
+                        </Toolbar>
+                    </AppBar>
+                    <NewBlog />
+                </Dialog>*/}
+            </div>
     );
-} */
+}
+export default Body;
--- src/Components/NewBlog.jsx
+++ src/Components/NewBlog.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import "./Styles/NewBlog.css";
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
@@ -12,145 +12,139 @@ import Typography from '@material-ui/core/Typography';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Header from './Header';
+import Footer from './Footer';
 
 const CssTextField = withStyles({
     root: {
-        fontFamily: 'Helvetica',
-        '& label.Mui-focused': {
-            color: 'rgb(62, 182, 182)',
-            fontWeight: 200,
+      fontFamily: 'Helvetica',
+      '& label.Mui-focused': {
+        color: 'rgb(62, 182, 182)',
+        fontWeight: 200,
+      },
+      '& .MuiInput-underline:after': {
+        borderBottomColor: 'rgb(62, 182, 182)',
+      },
+      '& .MuiOutlinedInput-root': {
+        '&.Mui-focused fieldset': {
+          borderColor: 'rgb(62, 182, 182)',
         },
-        '& .MuiInput-underline:after': {
-            borderBottomColor: 'rgb(62, 182, 182)',
-        },
-        '& .MuiOutlinedInput-root': {
-            '&.Mui-focused fieldset': {
-                borderColor: 'rgb(62, 182, 182)',
-            },
-        },
+    },
         boxShadow: 'none',
     },
-})(TextField);
-
+  })(TextField);
+  
 const useStyles = makeStyles((theme) => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(1),
-            width: '100ch',
-            fontFamily: 'arial',
-        },
-        '& .MuiTextField-root': {
-            margin: theme.spacing(1),
-            width: '100ch',
-            backgroundColor: theme.palette.common.white,
-            borderRadius: '.5rem',
-            outline: 'none',
-            boxShadow: '0 .8rem .8rem rgba(54, 186, 196, 0.2)',
-        },
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '100ch',
+      fontFamily: 'arial',
     },
+    '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        width: '100ch',
+        backgroundColor: theme.palette.common.white,
+        borderRadius: '.5rem',
+        outline: 'none',       
+    },
+  },
     formControl: {
         margin: theme.spacing(1),
         minWidth: 120,
-        boxShadow: '0 .8rem .8rem rgba(54, 186, 196, 0.2)',
         borderRadius: '.5rem',
         outline: 'none',
         fontFamily: 'Helvetica',
         '& label.Mui-focused': {
-            color: 'rgb(62, 182, 182)',
-            fontWeight: 200,
+        color: 'rgb(62, 182, 182)',
+        fontWeight: 200,
+      },
+      '& .MuiInput-underline:after': {
+        borderBottomColor: 'rgb(62, 182, 182)',
+      },
+      '& .MuiOutlinedInput-root': {
+        '&.Mui-focused fieldset': {
+          borderColor: 'rgb(62, 182, 182)',
         },
-        '& .MuiInput-underline:after': {
-            borderBottomColor: 'rgb(62, 182, 182)',
-        },
-        '& .MuiOutlinedInput-root': {
-            '&.Mui-focused fieldset': {
-                borderColor: 'rgb(62, 182, 182)',
-            },
-        },
+    },
     },
 }));
 
-async function updateDatabase(jsonData) {
-    const response = await fetch('http://localhost:9000/mongoData',
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(jsonData),
-        });
-    const body = await response.text();
-    console.log(body);
-}
-
 function NewBlog() {
     const classes = useStyles();
-    const [showClose, setShowClose] = useState(false);
+    const [showClose, setShowClose]= useState(false);
+    const [cat, setCat] = useState('');
 
-    const [openDialog, setOpenDialog] = useState(false);
-    var data = { title: '', desc: '', content: '', category: '' };
-
+    const handleChange = (event) => {
+      setCat(event.target.value);
+    };
+    const [openDialog, setOpenDialog] = useState(false); 
+    
     return (
-        <div >
-            <div className="newBlog" style={showClose ? { display: "none" } : { display: "flex" }} >
-                <div className="groupBackground">
-                    <form className={classes.root} noValidate autoComplete="off">
-                        <div className="title">
-                            <CssTextField required id="Title" label="Title" variant="outlined" style={{ width: "98ch" }}
-                                onChange={(e) => data.title = e.target.value} />
-                        </div>
-                        <div className="photo">
-                            <input type="file" id="photo" required></input>
-                        </div>
-                        <FormControl variant="outlined" className={classes.formControl}>
-                            <InputLabel id="catMenuLabel" required>Category</InputLabel>
-                            <Select
-                                labelId="catMenuLabel"
-                                id="catMenu"
-                                value={''}
-                                onChange={(e) => { data.category = e.target.value; }}
-                                label="Category">
-                                <MenuItem value="">
-                                    <em>None</em>
-                                </MenuItem>
-                                <MenuItem value={10}>Cat1</MenuItem>
-                                <MenuItem value={20}>Cat2</MenuItem>
-                                <MenuItem value={30}>Cat3</MenuItem>
-                                <MenuItem value={30}>Cat4</MenuItem>
-                            </Select>
-                        </FormControl>
-                        <div className="shortDescription">
-                            <CssTextField id="shortDes" label="Short Description" multiline rows={4} rowsMax={4} variant="outlined" margin="none" required
-                                style={{ width: "98ch" }} onChange={(e) => data.desc = e.target.value} />
-                        </div>
-                        <div className="content">
-                            <CssTextField id="Content" label="Content" multiline rows={20} variant="outlined" margin="none" required
-                                style={{ width: "98ch" }} onChange={(e) => data.content = e.target.value} />
-                        </div>
-
-                        <div className="urbutton">
-                            <a href="#"><button type="button" id="upload" onClick={() => { setOpenDialog(true); updateDatabase(data); }}>Upload</button></a>
-                            <button type="reset" id="reset">Reset</button>
-                        </div>
-                    </form>
-                </div>
+      <div >
+        <Header />
+        <div className="newBlog" style={showClose?{display:"none"}:{display:"flex"}} >
+            <div className="groupBackground">
+                <form className={classes.root} noValidate autoComplete="off">
+                    <div className="title">
+                        
+                        <CssTextField required id="Title" label="Title" variant="outlined" style = {{width: "98ch"}} 
+                                      helperText="This is the title that will be displayed at the top of your blog"/>
+                    </div>
+                    <div className="photo">
+                        <input type="file" id="photo" required></input>
+                    </div>
+                    <FormControl variant="outlined" className={classes.formControl}>
+                        <InputLabel id="catMenuLabel" required>Category</InputLabel>
+                        <Select
+                          labelId="catMenuLabel"
+                          id="catMenu"
+                          value={cat}
+                          onChange={handleChange}
+                          label="Category"
+                        >
+                          <MenuItem value="">
+                            <em>None</em>
+                          </MenuItem>
+                          <MenuItem value={10}>Cat1</MenuItem>
+                          <MenuItem value={20}>Cat2</MenuItem>
+                          <MenuItem value={30}>Cat3</MenuItem>
+                          <MenuItem value={30}>Cat4</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <div className="shortDescription">
+                        <CssTextField id="shortDes" label="Short Description" multiline rows={4} rowsMax={4} variant="outlined" margin="none" required 
+                                    style = {{width: "98ch"}} helperText="What's your blog about? Keep it short! :)"/>
+                    </div>
+                    <div className="content">
+                        <CssTextField id="Content" label="Content" multiline  rows={20} variant="outlined" margin="none" required style = {{width: "98ch"}}/>
+                    </div>  
+                
+                    <div className="urbutton">
+                        <a><button type="button" id="upload" onClick={()=>{setOpenDialog(true)}}>Upload</button></a>
+                        <button type="reset" id="reset">Reset</button>
+                   </div>
+                </form>
             </div>
-            <Dialog onClose={() => setOpenDialog(false)} open={openDialog} disableBackdropClick='true' disableEscapeKeyDown='true'>
+          </div>
+          <Dialog onClose={()=>setOpenDialog(false)} open={openDialog} disableBackdropClick='true' disableEscapeKeyDown='true'>
                 <DialogTitle id="uploadConfirm">
                     Success
                 </DialogTitle>
                 <DialogContent>
-                    <Typography gutterBottom variant="body1">Your blog has been uploaded!</Typography>
-                    <Typography gutterBottom> </Typography>
+                  <Typography gutterBottom variant="body1">Your blog has been uploaded!</Typography>
+                  <Typography gutterBottom> </Typography>
                 </DialogContent>
                 <DialogActions>
-                    <a href="#"><Button autoFocus color="primary">
-                        OK
+                  <a href="#"><Button autoFocus  color="primary">
+                      OK
                   </Button></a>
                 </DialogActions>
-            </Dialog>
-        </div>
+          </Dialog>
+          <Footer />
+      </div>
     );
-}
-
-export default NewBlog;
+  }
+  
+  export default NewBlog;
+  
